6bed4afc67a02c08b07cc6e2a9920a70e2bdc05b
cegli <ceglarzp@gmail.com>
2017-02-26 12:03:22 +0100

    adding openlayers to vendors, first initial app launch

--- WebContent/frontend/gulpfile.js
+++ WebContent/frontend/gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('init', ['scss-to-css']);
+gulp.task('init', ['scss-to-css', 'build-openlayers']);
 
 gulp.task('scss-to-css', function() {
 	return gulp.src('scss/**/*.scss')
 	.pipe(sass())
 	.pipe(gulp.dest('css'))
 });
+
+
+gulp.task('build-openlayers', function() {
+	return gulp.src('node_modules/openlayers/dist/*')
+	.pipe(gulp.dest('vendor/openlayers'))
+})
